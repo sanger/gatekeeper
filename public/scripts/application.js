46476530694836e0368754afeb1386f9867fa590
James Glover <jg16@sanger.ac.uk>
2014-02-03 12:03:46 +0000

    Find IDT plate dialog

--- public/scripts/application.js
+++ public/scripts/application.js
@@ -1,7 +1,12 @@
 (function(window,$,undefined) {
 
   /* Shared */
-
-  $('.menu-item').button();
+  $('.modal').on('shown.bs.modal', function (e) {
+    // Autofocus the data-focus element on show
+    console.log('called');
+    console.log(e.target.getAttribute('data-focus'));
+    console.log(document.getElementById(e.target.getAttribute('data-focus')));
+    document.getElementById(e.target.getAttribute('data-focus')).focus();
+  })
 
 })(window,jQuery,undefined)
