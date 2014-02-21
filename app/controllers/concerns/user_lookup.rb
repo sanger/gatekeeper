b699f4c92e3cf50feec885154c17c7a5795f3ace
James Glover <james.glover@sanger.ac.uk>
2014-02-21 11:08:56 +0000

    Document user_lookup.rb

--- app/controllers/concerns/user_lookup.rb
+++ app/controllers/concerns/user_lookup.rb
@@ -1,7 +1,15 @@
+##
+# Include in controllers to provide the #find_user filter
+# This filter both provides swipecard lookup, and prevents unauthorised
+# users from using the controller. Actions where a swipecard is not required
+# should be excluded with eg. skip_before_action :find_user, :except => [:create]
 module UserLookup
 
   private
 
+  ##
+  # Takes the user_swipecard parameter, and uses the api to find the corresponding user
+  # Redirects to root with an error flash if no swipecard is found, or if it can't find a user
   def find_user
     swipecard_code = params[:user_swipecard]
     return user_error("User swipecard must be provided.") if swipecard_code.nil?
@@ -13,10 +21,14 @@ module UserLookup
     end
   end
 
+  ##
+  # The client-api-gem currently only raises standard errors. We check the message to determine the content
   def user_not_found?(message)
     message.include?("no resources found with that search criteria")
   end
 
+  ##
+  # Redirect to the root with a flash[:danger] of message
   def user_error(message)
     flash[:danger] = message
     redirect_to :root
