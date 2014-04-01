77ff102fddf44a6185de1caf41ca4b1e9bdd8493
James Glover <james.glover@sanger.ac.uk>
2014-04-01 11:23:13 +0100

    Submission interface reports errors

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -6,22 +6,26 @@ class SubmissionsController < ApplicationController
   before_filter :find_asset_from_barcode
 
   def create
-    order = api.order_template.find(Settings.submission_templates.miseq).orders.create!(
-      :study => Settings.study,
-      :project => Settings.project,
-      :assets => [@asset.uuid],
-      :request_options => Gatekeeper::Application.config.request_options,
-      :user => @user.uuid
-    )
+    begin
+      order = api.order_template.find(Settings.submission_templates.miseq).orders.create!(
+        :study => Settings.study,
+        :project => Settings.project,
+        :assets => [@asset.uuid],
+        :request_options => Gatekeeper::Application.config.request_options,
+        :user => @user.uuid
+      )
 
-    submission = api.submission.create!(
-      :orders => [order.uuid],
-      :user => @user.uuid
-    )
+      submission = api.submission.create!(
+        :orders => [order.uuid],
+        :user => @user.uuid
+      )
 
-    submission.submit!
+      submission.submit!
 
-    render(:json=>{'success'=>'Submission created!'},:root=>true)
+      render(:json=>{'success'=>'Submission created!'},:root=>true)
+    rescue Sequencescape::Api::ResourceInvalid => exception
+      render(:json=>{'error'=>'Submission Failed. ' + exception.resource.errors.full_messages.join('; ') },:root=>true,:status=>403)
+    end
   end
 
   private
