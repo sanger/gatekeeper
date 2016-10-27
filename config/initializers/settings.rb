b945b4e10713941060afc7c458073757c82d727f
James Glover <james.glover@sanger.ac.uk>
2016-10-27 13:37:38 +0100

    Rubocop - remove unecessary splat

--- config/initializers/settings.rb
+++ config/initializers/settings.rb
@@ -10,7 +10,7 @@ class Settings
     protected :method_missing
 
     def configuration_filename
-      File.join(File.dirname(__FILE__), *%W[.. settings #{Rails.env}.yml])
+      File.join(File.dirname(__FILE__), '..', 'settings', "#{Rails.env}.yml")
     end
     private :configuration_filename
 
@@ -19,10 +19,10 @@ class Settings
       @instance = Hashie::Mash.new(YAML.load(ERB.new(File.read(configuration_filename)).result))
     rescue Errno::ENOENT => exception
       star_length = [ 96, 12+configuration_filename.length ].max
-      $stderr.puts('*'*star_length)
+      $stderr.puts('*' * star_length)
       $stderr.puts "WARNING! No #{configuration_filename}"
       $stderr.puts "You need to run 'rake config:generate' and can ignore this message if that's what you are doing!"
-      $stderr.puts('*'*star_length)
+      $stderr.puts('*' * star_length)
     end
   end
 end
