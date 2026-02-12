941591cbe81e0514e773cddd369d587999b67c2d
Stephen Hulme <sh51@sanger.ac.uk>
2026-02-12 15:35:47 +0000

    build: Rails 7.1 app:update and rubocop

--- config/application.rb
+++ config/application.rb
@@ -3,9 +3,13 @@
 require_relative 'boot'
 
 # We don't want ActiveRecord
+require 'rails'
+# Pick the frameworks you want:
+require 'active_model/railtie'
+require 'active_job/railtie'
 require 'action_controller/railtie'
 require 'action_mailer/railtie'
-require 'sprockets/railtie'
+require 'action_view/railtie'
 require 'rails/test_unit/railtie'
 require './lib/gatekeeper/version'
 
@@ -26,6 +30,19 @@ module Gatekeeper
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
+
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: %w[assets tasks])
+
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
