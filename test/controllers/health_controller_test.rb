c0dc875d307bef4524284384e5eeac5329c85b03
Stephen Hulme <sh51@sanger.ac.uk>
2026-02-26 22:07:34 +0000

    Add Sequencescape healthcheck to endpoint

--- test/controllers/health_controller_test.rb
+++ test/controllers/health_controller_test.rb
@@ -2,10 +2,43 @@
 
 require 'test_helper'
 
-class HealthControllerTest < ActionDispatch::IntegrationTest
-  test 'should get health' do
-    get health_url
+class HealthControllerTest < ActionController::TestCase
+  test 'returns OK when Sequencescape is available' do
+    test_conn = Faraday.new do |builder|
+      builder.adapter :test, Faraday::Adapter::Test::Stubs.new do |stub|
+        stub.get('/health') { |_env| [200, {}, 'OK'] }
+      end
+    end
+    @controller.stubs(:sequencescape_conn).returns(test_conn)
+
+    get :show
     assert_response :success
-    assert_equal 'OK', @response.body.strip
+    assert_equal 'OK', @response.body
+  end
+
+  test 'returns Gateway Timeout when Sequencescape connection times out' do
+    test_conn = Faraday.new do |builder|
+      builder.adapter :test, Faraday::Adapter::Test::Stubs.new do |stub|
+        stub.get('/health') { |_env| raise Faraday::TimeoutError }
+      end
+    end
+    @controller.stubs(:sequencescape_conn).returns(test_conn)
+
+    get :show
+    assert_response :gateway_timeout
+    assert_equal 'Connection to Sequencescape timed out', @response.body
+  end
+
+  test 'returns Bad Gateway when Sequencescape is unavailable' do
+    test_conn = Faraday.new do |builder|
+      builder.adapter :test, Faraday::Adapter::Test::Stubs.new do |stub|
+        stub.get('/health') { |_env| raise Faraday::ServerError, 'Internal server error' }
+      end
+    end
+    @controller.stubs(:sequencescape_conn).returns(test_conn)
+
+    get :show
+    assert_response :bad_gateway
+    assert_equal 'Sequencescape is unavailable', @response.body
   end
 end
