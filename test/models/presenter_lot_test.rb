10c6e28ae717030659f42a3eb4986cf77ce07c90
Shiv <44001656+borhara@users.noreply.github.com>
2026-03-17 12:58:35 +0000

    refactor: use v2 lot api for new tests

--- test/models/presenter_lot_test.rb
+++ test/models/presenter_lot_test.rb
@@ -8,26 +8,12 @@ class Presenter::LotTest < ActiveSupport::TestCase
 
   setup do
     mock_api
-    @lot_v1 = api.lot.find('11111111-2222-3333-4444-555555555556')
-    @presenter = Presenter::Lot.new(@lot_v1)
-  end
-
-  test '#prefix with V1/Mock style barcodes' do
-    assert_equal 'DN', @presenter.prefix
+    @lot_v2 = api.lot.find('11111111-2222-3333-4444-555555555556')
+    @presenter = Presenter::Lot.new(@lot_v2)
   end
 
   test '#prefix with V2 style barcodes' do
-    # Manually mock a qcable with labware_barcode
-    v2_qcable = mock('qcable')
-    v2_qcable.stubs(:labware_barcode).returns({ 'human_barcode' => 'XYZ123' })
-    v2_qcable.stubs(:respond_to?).with(:labware_barcode).returns(true)
-    v2_qcable.stubs(:respond_to?).with(:barcode).returns(false)
-
-    mock_lot = mock('lot')
-    mock_lot.stubs(:qcables).returns([v2_qcable])
-
-    presenter = Presenter::Lot.new(mock_lot)
-    assert_equal 'XYZ', presenter.prefix
+    assert_equal 'DN', @presenter.prefix
   end
 
   test '#prefix with empty qcables' do
--- test/models/presenter_qcable_test.rb
+++ test/models/presenter_qcable_test.rb
@@ -8,16 +8,35 @@ class Presenter::QcableTest < ActiveSupport::TestCase
 
   setup do
     mock_api
-    @qcable_v1 = api.qcable.find('11111111-2222-3333-4444-100000000001')
-    @presenter = Presenter::Qcable.new(@qcable_v1)
+    @qcable_v2 = api.qcable.find('11111111-2222-3333-4444-100000000001')
+    @presenter = Presenter::Qcable.new(@qcable_v2)
   end
 
-  test '#human_readable with V1/Mock style barcodes (machine)' do
+  test '#human_readable with V2 style barcodes (labware_barcode)' do
     assert_equal 'DN1S', @presenter.human_readable
   end
 
-  test '#human_readable with V1/Mock style barcodes (prefix + number fallback)' do
-    # Create a qcable mock that only has prefix and number
+  test '#human_readable falls back to barcode machine field (hash)' do
+    mock_qcable = mock('qcable')
+    mock_qcable.stubs(:respond_to?).with(:labware_barcode).returns(false)
+    mock_qcable.stubs(:respond_to?).with(:barcode).returns(true)
+    mock_qcable.stubs(:barcode).returns({ 'machine' => 'DN7B' })
+
+    presenter = Presenter::Qcable.new(mock_qcable)
+    assert_equal 'DN7B', presenter.human_readable
+  end
+
+  test '#human_readable falls back to barcode prefix + number (hash)' do
+    mock_qcable = mock('qcable')
+    mock_qcable.stubs(:respond_to?).with(:labware_barcode).returns(false)
+    mock_qcable.stubs(:respond_to?).with(:barcode).returns(true)
+    mock_qcable.stubs(:barcode).returns({ 'prefix' => 'DN', 'number' => '42' })
+
+    presenter = Presenter::Qcable.new(mock_qcable)
+    assert_equal 'DN42', presenter.human_readable
+  end
+
+  test '#human_readable falls back to barcode prefix + number (object)' do
     bc = mock('barcode')
     bc.stubs(:respond_to?).with(:machine).returns(false)
     bc.stubs(:respond_to?).with(:prefix).returns(true)
@@ -34,37 +53,8 @@ class Presenter::QcableTest < ActiveSupport::TestCase
     assert_equal 'DN1', presenter.human_readable
   end
 
-  test '#human_readable with V1/Mock style barcodes (hash)' do
-    mock_qcable = mock('qcable')
-    mock_qcable.stubs(:respond_to?).with(:labware_barcode).returns(false)
-    mock_qcable.stubs(:respond_to?).with(:barcode).returns(true)
-    mock_qcable.stubs(:barcode).returns({ 'machine' => 'DN7B' })
-
-    presenter = Presenter::Qcable.new(mock_qcable)
-    assert_equal 'DN7B', presenter.human_readable
-  end
-
-  test '#human_readable with V1/Mock style barcodes (hash prefix + number)' do
-    mock_qcable = mock('qcable')
-    mock_qcable.stubs(:respond_to?).with(:labware_barcode).returns(false)
-    mock_qcable.stubs(:respond_to?).with(:barcode).returns(true)
-    mock_qcable.stubs(:barcode).returns({ 'prefix' => 'DN', 'number' => '42' })
-
-    presenter = Presenter::Qcable.new(mock_qcable)
-    assert_equal 'DN42', presenter.human_readable
-  end
-
-  test '#human_readable with V2 style barcodes' do
-    mock_qcable = mock('qcable')
-    mock_qcable.stubs(:respond_to?).with(:labware_barcode).returns(true)
-    mock_qcable.stubs(:labware_barcode).returns({ 'human_barcode' => 'ABC456' })
-
-    presenter = Presenter::Qcable.new(mock_qcable)
-    assert_equal 'ABC456', presenter.human_readable
-  end
-
   test '#index' do
-    assert_equal '-', @presenter.index # DN1S lamp_index is nil
+    assert_equal '-', @presenter.index # stamp_index is nil
   end
 
   test '#index with value' do
