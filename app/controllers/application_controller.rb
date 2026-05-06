1cc42f2eecf7c2d2550dab268ad2819dc41d76d3
Stephen Hulme <sh51@sanger.ac.uk>
2026-05-06 15:59:39 +0100

    fix: remove api v1 config and initial connection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,16 +8,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  include Sequencescape::Api::Rails::ApplicationController
   include UserError
   include UserLookup
   include ApiError
 
   private
-
-  def api_connection_options
-    Gatekeeper::Application.config.api_connection_options
-  end
 
   rescue_from Errno::ECONNREFUSED, with: :sequencescape_down
 
