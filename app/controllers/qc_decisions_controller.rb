4ce5983e24d9613b9359f5147b516037f6350f28
Stephen Hulme <sh51@sanger.ac.uk>
2026-06-25 14:42:05 +0100

    fix: remove more unused code

--- app/controllers/qc_decisions_controller.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class QcDecisionsController < ApplicationController
-  include BatchLookup
-
-  def find_lot_presenter
-    @lot_presenter = Presenter::Lot.new(Sequencescape::Api::V2::Lot.find(params[:lot_id]))
-  end
-end
