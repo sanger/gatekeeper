408826dfb51002d6a69562aecc37f5641cf60b01
Stephen Hulme <sh51@sanger.ac.uk>
2026-07-02 13:40:08 +0100

    test: fix missing hash

--- test/models/sequencescape_api_v2_base_test.rb
+++ test/models/sequencescape_api_v2_base_test.rb
@@ -5,7 +5,7 @@ require 'test_helper'
 class SequencescapeApiV2BaseTest < ActiveSupport::TestCase
   test 'find! returns records when find is not empty' do
     record = mock('record')
-    Sequencescape::Api::V2::Base.stubs(:find).with(id: '123').returns([record])
+    Sequencescape::Api::V2::Base.stubs(:find).with({ id: '123' }).returns([record])
 
     result = Sequencescape::Api::V2::Base.find!(id: '123')
 
@@ -13,7 +13,7 @@ class SequencescapeApiV2BaseTest < ActiveSupport::TestCase
   end
 
   test 'find! raises not found when find is empty' do
-    Sequencescape::Api::V2::Base.stubs(:find).with(id: 'missing').returns([])
+    Sequencescape::Api::V2::Base.stubs(:find).with({ id: 'missing' }).returns([])
 
     error = assert_raises(JsonApiClient::Errors::NotFound) do
       Sequencescape::Api::V2::Base.find!(id: 'missing')
