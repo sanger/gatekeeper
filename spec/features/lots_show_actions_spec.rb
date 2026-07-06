6c5d5e6eb909907c00783fbe01acf5ff82ab1dee
Stephen Hulme <sh51@sanger.ac.uk>
2026-07-06 14:13:07 +0100

    test: add user where search into v2 mocks file

--- spec/features/lots_show_actions_spec.rb
+++ spec/features/lots_show_actions_spec.rb
@@ -75,8 +75,6 @@ RSpec.describe 'Lot show actions', type: :feature, js: true do
     printer.uuid = 'baac0dea-0000-0000-0000-000000000000' if printer.respond_to?(:uuid=)
 
     allow(Sequencescape::Api::V2::Lot).to receive(:where).with(uuid: lot_uuid).and_return([shown_lot])
-    allow(Sequencescape::Api::V2::User).to receive(:where).with(uuid: '11111111-2222-3333-4444-555555555555')
-                                                          .and_return([Sequencescape::Api::V2::User.new])
     expect(Sequencescape::Api::V2::QcableCreator).to receive(:new).with({ count: 2 }).and_return(qcable_creator)
     allow(qcable_creator).to receive(:save).and_return(true)
     allow(qcable_creator).to receive(:qcables).and_return(created_qcables)
