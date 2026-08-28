9e9e206bdbed082c4653e2200dd672686d4c78e2
Stephen Hulme <sh51@sanger.ac.uk>
2026-08-28 16:43:30 +0100

    fix: when all resources are requested, iterate through returned pages

--- app/resources/sequencescape/api/v2/base.rb
+++ app/resources/sequencescape/api/v2/base.rb
@@ -20,4 +20,27 @@ class Sequencescape::Api::V2::Base < JsonApiClient::Resource
 
     record
   end
+
+  # When fetching all records, iteratively fetch each next page and combine them into a single result set.
+  def self.all
+    result_set = super # first page
+
+    while next_page?(result_set)
+      next_result_set = result_set.pages.next
+      result_set.concat(next_result_set)
+      result_set = next_result_set
+    end
+
+    result_set
+  end
+
+  # Check if the given result set has a next page of results.
+  # @param result_set [JsonApiClient::ResultSet] the result set to check for a next page
+  # @return [Boolean] true if there is a next page, false otherwise
+  def self.next_page?(result_set)
+    result_set.links.link_url_for('next')
+    true
+  rescue KeyError
+    false
+  end
 end
--- test/models/sequencescape_api_v2_base_test.rb
+++ test/models/sequencescape_api_v2_base_test.rb
@@ -19,4 +19,44 @@ class SequencescapeApiV2BaseTest < ActiveSupport::TestCase
       Sequencescape::Api::V2::Base.find!(id: 'missing')
     end
   end
+
+  test 'all fetches all pages of results' do
+    # While not representing actual objects, this is a good high-level summary of how the page fetching works
+    result_set_1 = ['item1.1', 'item1.2', 'item1.3']
+    result_set_2 = ['item2.1', 'item2.2', 'item2.3']
+    result_set_3 = ['item3.1', 'item3.2', 'item3.3']
+
+    third_page = build_page(result_set_3, next_page_url: nil)
+    second_page = build_page(result_set_2, next_page: third_page, next_page_url: 'url_3')
+    first_page = build_page(result_set_1, next_page: second_page, next_page_url: 'url_2')
+
+    JsonApiClient::Resource.stubs(:all).returns(first_page)
+
+    result = Sequencescape::Api::V2::Base.all
+
+    expected = result_set_1 + result_set_2 + result_set_3
+    assert_equal expected, result
+  end
+
+  def build_page(result_set, next_page: nil, next_page_url: nil) # rubocop:disable Metrics/MethodLength
+    page = result_set.dup
+
+    page_pages = stub('pages')
+    page_pages.stubs(:next).returns(next_page)
+    page_links = stub('links')
+    if next_page_url
+      page_links.stubs(:link_url_for).with('next').returns(next_page_url)
+    else
+      page_links.stubs(:link_url_for).with('next').raises(KeyError)
+    end
+
+    page.define_singleton_method(:pages) { page_pages }
+    page.define_singleton_method(:links) { page_links }
+    page.define_singleton_method(:concat) do |other_page|
+      other_page.replace(self + other_page)
+      other_page
+    end
+
+    page
+  end
 end
